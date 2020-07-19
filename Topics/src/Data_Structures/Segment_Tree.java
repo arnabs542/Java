fa4d7affc125e007ec36e76fe3cb7045894dc738
AdmiJW <jamweg22@gmail.com>
2020-07-19 16:26:48 +0800

    Added Segment Tree - Lazy Propagation

--- Topics/src/Data_Structures/Segment_Tree.java
+++ Topics/src/Data_Structures/Segment_Tree.java
@@ -1,27 +1,83 @@
 package Data_Structures;
 
+import java.util.Arrays;
+
 /*
+ * 	Segment Tree is a binary tree based data structure which is implemented preferably using an array. Its use is to enable efficient
+ * 	querying of an array like the max, min or sum without doing it in O(N) time. In fact, using a segment tree, the max, min or sum
+ * 	of the entire or part of the array can be found in O(log n) time. This is great if the query is asked many times over and over again.
  * 
+ * 	First of all, the size of the segment tree were to be determined. Since it basically is binary tree, it will be the
+ * 	nextPow2( arr.length) * 2 - 1 sized. Eg: The next power 2 of 9 is 16, and the next power 2 of 8 is 8 itself.
+ * 	Therefore for array of size 9, the size of tree should be 16 * 2 - 1 = 31.
+ * 	In case where the arr.length is not exactly a power of 2, some of the array spaces will be ignored and never accessed
+ * 	(Those spaces are non-existent nodes in the binary tree)
+ * 
+ * 	Each node in the segment tree will represent the answer for the query in certain range.	The head or root node, will be the query answer
+ * 	for the entire array, which is index from 0 to arr.length - 1.
+ * 	For example, for the array [1,2,3,4,5,6] of length 6, and segment tree is based on query of maximum value, then the root node
+ * 	will represent the answer to "The maximum value for the array from index 0 to 5", which should hold 6
+ * 
+ * 	The child nodes will have the ranges split in half. From root node of example above, the range is from index 0 to 5. If split in half,
+ * 	then the left child node shall represent the query answer for range 0 to 2, and right child node shall represent answer for range
+ * 	3 to 5. This propagation continues until the left and right range become equal, which represent only single element in the array.
+ * 
+ * 	Both the construction and querying of segment tree is mainly recursive.
+ * 	During construction, we shall check if the left range is equal to the right range. If it is, that means the range only consisted of a
+ * 	single element and the answer to the query is the element itself. Therefore we would just place the element value in this node
+ * 	and return.
+ * 	If the left range is not equal to right range, then the left child and the right child needs to be constructed first using recursive call, 
+ *  then only compare the left child and right child's value to obtain the answer for this node.
+ *  
+ *  
+ *  When doing query searches, we have to keep track of the query left range, query right range, current node's left range and right range,
+ *  and the current position in the segment tree.
+ *  The query range will always be in one of three situations below:
+ *  	-Partial Overlap: The query range is partially overlapping the current node range. We need to search both the left and right child,
+ *  					  and return the minimum/maximum/sum of the two (Based on type of query)
+ *  	-No Overlap: The query range does not overlap with current node range at all. This node value has nothing to do with the current
+ *  				 query. So return either some MAX value or MIN value or special value (Based on type of query)
+ *  	-Total Overlap: The query range is fully covering the current node range. There is no need to search further.
+ *  					 Therefore just return this node's value to be further compared by the upper layer recursive call.
+ *  
+ *  Every call, we have to calculate the node's left range, node's right range, and the respective position (index) of the segment tree
+ *  of the node, and should perform respective action based on the 3 situations above.
+ *  
+ *  If we want to change a particular value in the array, this could be done by traversing to the node which represent that single element
+ *  alone, updating the value, and for parent nodes, compare the child nodes again to update the value.
  */
 
+
+//Example implementation of a minimum Segment tree. Provides minimum value as query type
 public class Segment_Tree {
 
 	static int arrlength = -1;
 	static int[] segmentTree = null;
 	
+	
 	public static void createSegmentTree(int[] arr ) {
 		segmentTree = new int[ getPower2(arr.length) * 2 - 1];
 	
-		createRecurse(arr, 0, arr.length - 1, 0);
+		createSegmentTree(arr, 0, arr.length - 1, 0);
+		
 		arrlength = arr.length;
 	}
 	
-	private static int minimumRange(int left, int right) {
+	public static int minimumRange(int left, int right) {
 		return searchQuery(segmentTree, 0, arrlength - 1, left, right, 0);
 	}
 	
-	public static int searchQuery(int[] arr, int left, int right, int qleft, int qright, int pos) {
+	public static void changeValue(int pos, int newVal) {
+		changeValue(pos, newVal, 0, arrlength - 1, 0);
+	}
+	
+	//----------------------------------------------------------------------------------------------------
+	// PRIVATE HELPER METHODS
+	//----------------------------------------------------------------------------------------------------
+	
+	private static int searchQuery(int[] arr, int left, int right, int qleft, int qright, int pos) {
 		System.out.println("Search " + left + ", " + right);
+		
 		//Total overlap. Return this value in array is enough
 		if (left >= qleft && qright >= right) {
 			System.out.println("Return TOTAL " + arr[pos]);
@@ -41,9 +97,9 @@ public class Segment_Tree {
 		System.out.println("Return PARTIAL " + res);
 		return res;
 	
-	}
+	}	//end of searchQuery()
 	
-	private static void createRecurse(int[] arr, int left, int right, int pos) {
+	private static void createSegmentTree(int[] arr, int left, int right, int pos) {
 		if (left == right) {
 			segmentTree[pos] = arr[left];
 			return;
@@ -51,11 +107,30 @@ public class Segment_Tree {
 		
 		int mid = left + (right - left) / 2;
 		
-		createRecurse(arr, left, mid, 2*pos + 1);
-		createRecurse(arr, mid + 1, right, 2*pos + 2);
+		createSegmentTree(arr, left, mid, 2*pos + 1);
+		createSegmentTree(arr, mid + 1, right, 2*pos + 2);
 		segmentTree[pos] = Math.min( segmentTree[2*pos + 1], segmentTree[2*pos + 2] );
 		
-	}
+	}	//end of createSegmentTree()
+	
+	private static void changeValue(int pos, int val, int left, int right, int treepos) {
+		//Total overlap (The position is found, update the value)
+		if (left == right && left == pos) {
+			segmentTree[treepos] = val;
+			return;
+		}
+		//No overlap. Just return this value to be compared
+		if (left > pos || right < pos) return;
+		
+		//Inside range. Find out which to find and compare them
+		int mid = left + (right - left ) / 2;
+		
+		if (pos > mid) changeValue(pos, val, mid + 1, right, treepos*2 + 2);
+		else changeValue(pos, val, left, mid, treepos*2 + 1);
+		
+		segmentTree[treepos] = Math.min( segmentTree[treepos*2 + 1], segmentTree[treepos*2 + 2] );
+		
+	}	//end of changeValue()
 	
 	
 	//Given argument i, will return the nearest greater power 2 of i. Eg: i is 9, the nearest greatest power of 2 is 16. If i is 16, return 16
@@ -84,7 +159,15 @@ public class Segment_Tree {
 		}
 		System.out.println();
 		
-		System.out.println("\n" + minimumRange(0, 5) );
+		System.out.println( minimumRange(0, 5) );
+		
+		changeValue(3, -5);
+		
+		for (int i: segmentTree) {
+			System.out.print(i + ", ");
+		}
+		
+		System.out.println( minimumRange(0, 5) );
 	}
 	
 }
---
+++ Topics/src/Data_Structures/Segment_Tree_Lazy_Propagating.java
@@ -0,0 +1,185 @@
+package Data_Structures;
+
+/*
+ * 	Segment tree is good by itself, but suppose the segment tree has to be updated frequently:
+ *  We will have to travel through the tree, reach until the bottom (Where the leaf node representing one individual element resides),
+ * 	update the value for it, and go back up, comparing the child nodes value and updating the node each time. This can be quite inefficient
+ * 	if the updating is done very frequently.
+ * 
+ * 	One optimization is to implement Lazy Propagation. This technique requires an additional tree which is same size as the segment tree
+ * 	itself. Initially, the lazy tree will be initialized with all values being 0, meaning no update has to be done.
+ * 
+ * 	When a range of indexes has to be updated with some value, we would perform a search similar to querySearch, but this time the key
+ * 	is when the query range TOTAL overlaps the current range.
+ * 	Let's say the query type is MINIMUM and each value has to be incremented by constant value. Once we found the range that TOTAL overlaps
+ * 	the query range, then we know for sure each value is going to be incremented by that constant, and the MINIMUM value for that range
+ * 	will be incremented by the constant as well. Therefore, change the value of current node by the constant.
+ * 	We won't be traveling further down the tree to update the further sub range's value. Instead, we will put the constant value to be
+ * 	updated into the lazy tree. The value of the lazy tree means the value awaiting to be updated for the respective node in the segment
+ * 	tree.
+ * 	Then, back to the upper layer of recursion, compare the child nodes, which is updated, and update the node's value, and return.
+ * 
+ * 	With this, every time we travel to a certain node, we have to check if the node's value were to be updated first, before even
+ * 	performing operations on it. If the node value was found to be updated from lazy tree, update the value, pass the update to
+ * 	child nodes (Through lazy tree), and reset the current node's lazy tree value to 0, meaning it was well updated.
+ * 
+ * 	
+ */
+
+//Example implementation of lazy propagating segment tree of type MINIMUM
+public class Segment_Tree_Lazy_Propagating {
+
+	static int[] segmentTree = null;
+	static int[] lazyTree = null;
+	static int length = -1;
+	
+	public static void createSegmentTree(int[] arr) {
+		length = arr.length;
+		
+		segmentTree = new int[getPower2(length) * 2 - 1 ];
+		lazyTree = new int[ segmentTree.length ];
+		
+		createSegmentTree(arr, 0, length - 1, 0);
+	}
+	
+	public static int querySearch(int qleft, int qright) {
+		return querySearch(0, length - 1, qleft, qright, 0);
+	}
+	
+	public static void changeRange(int qleft, int qright, int delta) {
+		changeRange(0, length - 1, qleft, qright, 0, delta);
+	}
+	
+	//------------------------------------------
+	//	PRIVATE HELPER METHODS
+	//------------------------------------------
+	
+	private static void createSegmentTree(int[] arr, int left, int right, int pos) {
+		//Leaf node. Just put the individual element into the tree and return
+		if (left == right) {
+			segmentTree[pos] = arr[left];
+			return;
+		}
+		
+		//Not leaf node. Left child node and right child node has to be constructed first before deciding the value of this node
+		int mid = left + (right - left) / 2;
+		createSegmentTree(arr, left, mid, pos*2 + 1);
+		createSegmentTree(arr, mid+1, right, pos*2 + 2);
+		
+		segmentTree[pos] = Math.min( segmentTree[pos*2 + 1],
+				segmentTree[pos*2 + 2] );
+	}	//end of createSegmentTree()
+	
+	
+	private static int querySearch(int left, int right, int qleft, int qright, int pos) {
+		
+		ensureUpdate(pos, left == right);
+		
+		//Total overlap
+		if (qleft <= left && right <= qright) {
+			return segmentTree[pos];
+		}
+		//No overlap
+		if (qright < left || qleft > right )
+			return Integer.MAX_VALUE;
+		
+		int mid = left + (right - left) / 2;
+		
+		return Math.min( querySearch(left, mid, qleft, qright, pos*2 + 1),
+						 querySearch(mid + 1, right, qleft, qright, pos*2 + 2) );	
+	}	//end of querySearch()
+	
+	
+	private static void changeRange(int left, int right, int qleft, int qright, int pos, int delta) {
+		
+		ensureUpdate(pos, left == right);
+		
+		//Total overlap. All node values below this were to be updated. Therefore update this node's value as well as put awaiting updates
+		//into lazy tree. Return.
+		if (qleft <= left && right <= qright) {
+			segmentTree[pos] += delta;
+			if (left != right) {
+				lazyTree[pos*2 + 1] += delta;
+				lazyTree[pos*2 + 2] += delta;
+			}
+			return;
+		}
+		//No overlap. No updates to any of node values
+		if (qright < left || qleft > right) return;
+		
+		//Partial overlap. Pass the incrementing to both sides since some nodes have to be updated while some don't
+		int mid = left + (right - left) / 2;
+		changeRange(left, mid, qleft, qright, pos*2 + 1, delta);
+		changeRange(mid + 1, right, qleft, qright, pos*2 + 2, delta);
+		
+		segmentTree[pos] = Math.min( segmentTree[pos*2 + 1],
+									 segmentTree[pos*2 + 2] );
+	}	//end of changeRange()
+	
+	
+	private static void ensureUpdate(int pos, boolean isLeaf) {
+		//No updates
+		if (lazyTree[pos] == 0 ) return;
+		
+		//Update segment tree value
+		segmentTree[pos] += lazyTree[pos];
+		
+		if (!isLeaf) {
+			lazyTree[pos*2 + 1] += lazyTree[pos];
+			lazyTree[pos*2 + 2] += lazyTree[pos];
+		}
+			
+		lazyTree[pos] = 0;
+	}	//end of ensureUpdate()
+	
+	
+	private static int getPower2(int i) {
+		if (i <= 0) return 0;
+		int copy = i;
+		
+		int count = 0;
+		while (i > 0) {
+			i >>>= 1;
+			count ++;
+		}
+		int potential = (1 << count);
+		
+		return (potential / copy == 2)? copy: potential;
+	}	//end of getPower2()
+	
+	
+	private static void printTrees() {
+		System.out.print("Segment Tree: ");
+		for (int i: segmentTree)
+			System.out.print(i + " ");
+		System.out.println();
+		
+		System.out.print("Lazy Tree: ");
+		for (int i: lazyTree)
+			System.out.print(i + " ");
+		System.out.println();
+	}
+	
+	//------------------------------------------------------------------------------
+	
+	public static void main(String[]args) {
+		int[] arr = {-1, 2, 4, 1, 7, 1, 3, 2};
+		
+		createSegmentTree(arr);
+		printTrees();
+		
+		changeRange(0, 3, 3);
+		printTrees();
+		
+		changeRange(0, 3, 1);
+		printTrees();
+		
+		changeRange(0, 0, 2);
+		printTrees();
+		
+		System.out.println( querySearch(3, 5) );
+		printTrees();
+		
+	}
+	
+}
