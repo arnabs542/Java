688101f2af0d21324fdca7ee992ec50a9da9a688
AdmiJW <jamweg22@gmail.com>
2020-11-01 16:28:07 +0800

    Added Convert Binary Number in a Linked List to Integer

---
+++ LeetCode/src/Easy/Convert_Binary_Number_in_a_Linked_List_To_Integer.java
@@ -0,0 +1,57 @@
+package Easy;
+
+//https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+
+/*
+ * 	THis is a bit manipulation - Linked list problem.
+ * 
+ * 	When we traverse the linked list, we traverse from rightmost to leftmost. This means the digit we met earlier in the
+ * 	linked list should be increase its digit position by 1 when we traverse to the next node.
+ * 	Recall from binary representation, each position is 2^1, 2^2, 2^3 etc. This means that we have options:
+ * 
+ * 	>	Use leftshift operator, which basically shift all bits to left by one
+ * 	>	Use *2 every time we traverse the node.
+ * 
+ */
+
+public class Convert_Binary_Number_in_a_Linked_List_To_Integer {
+	
+	//	Bit manipulation Iterative method
+	
+	public int getDecimalValue(ListNode head) {
+        int res = 0;
+        while (head != null) {
+        	res += head.val;
+        	res <<= 1;
+        	head = head.next;
+        }
+        
+        return res;
+    }
+	
+	//	Recursive Method
+	
+	public int getDecimalValueRecursion(ListNode head) {
+		return getDecimalValueRecursive(head, 0);
+	}
+	public int getDecimalValueRecursive(ListNode head, int val) {
+        if (head == null) return val;
+
+        val <<= 1;
+        val += head.val;
+        
+        return getDecimalValueRecursive(head.next, val);
+    }
+	
+	//	Math method
+	
+	public int getDecimalValueMath(ListNode head) {
+		int res = 0;
+		while ( head!= null) {
+			res *= 2;
+			res += head.val;
+			head = head.next;
+		}
+		return res;
+	}
+}
