3096e88725cc43b8e96256e867768974cf97a8ae
AdmiJW <jamweg22@gmail.com>
2021-01-12 16:43:51 +0800

    Improved Add Two Numbers

--- LeetCode/src/Medium/Add_Two_Numbers.java
+++ LeetCode/src/Medium/Add_Two_Numbers.java
@@ -1,84 +1,59 @@
 package Medium;
+import Linked_List.ListNode;
 
 //https://leetcode.com/problems/add-two-numbers/
+/*
+ * 	This is a linked list problem.
+ * 
+ * 	If we want, we can also do it in place, Taking O(1) space. But we won't here
+ * 
+ * 	The idea is direct: Using two pointers, add the two nodes. If exceed 10,
+ * 	store it into carry variable. The next node, the carry need to be added.
+ * 
+ * 	At the end, if the carry is still true, add a new node of 1.
+ */
 
 public class Add_Two_Numbers {
 
-	static class ListNode {
-	      int val;
-	      ListNode next;
-	      ListNode() {}
-	      ListNode(int val) { this.val = val; }
-	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-	  }
-	static class Solution {
-	    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-	       ListNode head = new ListNode(l1.val + l2.val , null);
-	       ListNode current = head;
-	       int pending = 0;
-	       if (head.val >= 10) {
-	    	   pending = head.val / 10;
-	    	   head.val = head.val % 10;
-	       }
-	       l1 = l1.next;
-	       l2 = l2.next;
-	       
-	       while (l1 != null && l2 != null) {
-	    	   ListNode node = new ListNode(pending + l1.val + l2.val, null);
-	    	   pending = 0;
-	    	   if (node.val >= 10) {
-		    	   pending = node.val / 10;
-		    	   node.val = node.val % 10;
-		       }
-	    	   current.next = node;
-	    	   current = node;
-	    	   l1 = l1.next;
-	    	   l2 = l2.next;
-	       }
-	       //List 2 exhausted
-	       while (l1 != null) {
-	    	   ListNode node = new ListNode(pending + l1.val, null);
-	    	   pending = 0;
-	    	   if (node.val >= 10) {
-		    	   pending = node.val / 10;
-		    	   node.val = node.val % 10;
-		       }
-	    	   current.next = node;
-	    	   current = node;
-	    	   l1 = l1.next;
-	       }
-	       
-	     //List 1 exhausted
-	       while (l2 != null) {
-	    	   ListNode node = new ListNode(pending + l2.val, null);
-	    	   pending = 0;
-	    	   if (node.val >= 10) {
-		    	   pending = node.val / 10;
-		    	   node.val = node.val % 10;
-		       }
-	    	   current.next = node;
-	    	   current = node;
-	    	   l2 = l2.next;
-	       }
-	       
-	       if (pending != 0) {
-	    	   ListNode node = new ListNode(pending, null);
-	    	   current.next = node;
-	       }
-	       
-	       return head;
-	    }
-	}
-	
-	public static void main(String[]args) {
-		ListNode n = new ListNode(1, new ListNode(2, new ListNode(3) ) );
-		ListNode n2 = new ListNode(4, new ListNode(5, new ListNode(6) ) );
-		ListNode sol = Solution.addTwoNumbers(n, n2);
-		while (sol != null) {
-			System.out.println(sol.val);
-			sol = sol.next;
+	//	Iterative method
+	ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode();
+		ListNode tail = dummy;
+		
+		boolean carry = false;
+		
+		while (l1 != null || l2 != null) {
+			int val = (l1 != null? l1.val: 0) + (l2 != null? l2.val: 0) + (carry? 1:0);
+			carry = val >= 10;
+			val %= 10;
+			
+			tail.next = new ListNode(val);
+			tail = tail.next;
+			
+			l1 = l1 == null? l1: l1.next;
+			l2 = l2 == null? l2: l2.next;
 		}
-		System.out.println( );
+		
+		if (carry) tail.next = new ListNode(1);
+		return dummy.next;
 	}
 	
+	
+	//	Recursive Method
+	ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+		return recurse(l1, l2, false);
+	}
+	
+	ListNode recurse(ListNode l1, ListNode l2, boolean carry) {
+		if (l1 == null && l2 == null)
+			return carry? new ListNode(1): null;
+			
+		int sum = (l1 != null? l1.val: 0) + (l2 != null? l2.val: 0) + (carry? 1: 0);
+		
+		ListNode toAppend = recurse(l1 == null? null: l1.next, l2 == null? null: l2.next, sum >= 10);
+		ListNode newNode = new ListNode(sum % 10);
+		newNode.next = toAppend;
+		
+		return newNode;
+	}
 }
