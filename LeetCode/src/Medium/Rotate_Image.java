02f203119bc73d61016916758ae9970aa80d404f
AdmiJW <jamweg22@gmail.com>
2021-04-25 17:54:09 +0800

    Added Rotate Image

---
+++ LeetCode/src/Medium/Rotate_Image.java
@@ -0,0 +1,83 @@
+package Medium;
+
+//https://leetcode.com/problems/rotate-image/
+/*
+ * 	This is an array trick problem
+ * 
+ * 	Try to rotate the matrix layer by layer, like an onion.
+ * 	The offset indicates which layer we are trying to rotating. Offset = 0 means the outermost layer
+ * 
+ * 	In each layer, since we have to do it inplace, each element rotating must make a full cycle to ensure
+ * 	no inforamtion is lost.
+ * 	Eg if I have matrix of size 2, then to rotate, start at (0,0), then (0,1), then (1,1), then (1,0). No information
+ * 	is lost
+ * 	
+ * 	The elements that has to be rotated in that layer is the ones in column less than (length - offset - 1)
+ * 	Eg: Square matrix of size 3x3. Then in outermost layer, element at col 0 and col 1 must be rotated. the rest will
+ * 	automatically be rotated due to rotation at previous element.
+ * 
+ * 	From a position (r,c), we map to the new position via (r,c)->(c, l - r - 1) where l is length of matrix
+ * 
+ * 	Once done, recurse (or iterate) into deeper 1 level. The layer cannot exceed or equal (length / 2)
+ * 
+ * 	-----------------------------------
+ * 
+ * 	In reality however, rotation is more likely to done via transformation matrix of rotation. Since each images are simply
+ * 	a point (x,y), it is simply a matrix multiplication with row 2.
+ * 
+ * 	----------------------------------
+ * A more elegant yet hard to observe idea, is to perform transpose, then reverse rows
+ * The reason why this works, is because from earlier formula,
+ * 			(r,c)-> (c, l - r - 1)
+ * 
+ * 	First, transposing flips r and c. So position becomes (c,r)
+ * 	Now, reversing the row will cause the second element to become l - e - 1. Hey! That's what we talking about!
+ * 			(c, l - r - 1)
+ */
+
+public class Rotate_Image {
+	
+	public void rotate(int[][] matrix) {
+		rotate(matrix, 0);
+    }
+	
+	//	Slightly recursive approach. It can be easily changed to iterative approach
+	private void rotate(int[][] matrix, int offset) {
+		if (offset >= matrix.length / 2) return;
+		for (int i = offset; i < matrix.length - offset - 1; ++i) {
+			int temp = 0, r = offset, c = i;
+			for (int j = 0; j <= 4; ++j) {
+				int val = matrix[r][c];
+				matrix[r][c] = temp;
+				temp = val;
+				
+				int oldr = r;
+				r = c;
+				c = matrix.length - oldr - 1;
+			}
+		}
+        rotate(matrix, offset+1);
+	}
+	
+	
+	//	Elegant way
+	public void rotate2(int[][] matrix) {
+		//	Transposing
+		for (int i = 0; i < matrix.length; ++i) {
+			for (int j = 0; j < i; ++j) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = temp;
+			}
+		}
+		
+		//	Reversing row
+		for (int i = 0; i < matrix.length; ++i) {
+			for (int j = 0; j < matrix.length / 2; ++j) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[i][matrix.length - j - 1];
+				matrix[i][matrix.length - j - 1] = temp;
+			}
+		}
+	}
+}
