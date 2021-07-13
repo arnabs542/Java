e326b2cc31e52061006b5bf0274021e8ccd1e801
AdmiJW <jamweg22@gmail.com>
2021-07-13 17:51:58 +0800

    Added Find Peak Element

---
+++ LeetCode/src/Medium/Find_Peak_Element.java
@@ -0,0 +1,34 @@
+package Medium;
+
+//https://leetcode.com/problems/find-peak-element/
+/*
+ * 	This is a binary search problem.
+ * 
+ * 	Given a middle element from possible range [l, r], say at index m.
+ *  We check whether it is a peak element or not. If not, there are several possible cases:
+ *  	>	HI X HI
+ *  	>	HI X LO
+ *  	>	LO X HI
+ *  
+ *  In this cases, we will always narrow the range to search towards the side with higher element. This is valid because
+ *  by following the trend of going higher, we will eventually reach the peak.
+ */
+
+public class Find_Peak_Element {
+	
+	// O(Log N) solution
+	public int findPeakElement(int[] nums) {
+		final int len = nums.length;
+        int l = 0, r = len - 1;
+        while (l < r) {
+        	int mid = l + (r - l) / 2;
+
+        	if (nums[mid] > nums[mid+1])		// Current element is larger. Next element is smaller
+        		r = mid;
+        	else
+        		l = mid+1;
+        }
+        return l;
+    }
+	
+}
