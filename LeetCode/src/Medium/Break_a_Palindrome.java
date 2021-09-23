b00c550d4f767b182cce95e0c1e7829d61224ca8
AdmiJW <jamweg22@gmail.com>
2021-09-23 15:39:38 +0800

    Added Break A Palindrome

---
+++ LeetCode/src/Medium/Break_a_Palindrome.java
@@ -0,0 +1,35 @@
+package Medium;
+
+//https://leetcode.com/problems/break-a-palindrome/
+/*
+ * This is a greedy string problem - The changes we made are always optimal to the problem.
+ * 
+ * First let's identify the base case - Any string of length 1 is always palindrome no matter how we change it.
+ * In a palindrome string, changing any character EXCEPT FOR THE MIDDLE CHARACTER IN A ODD LENGTH PALINDROME will
+ * always break the palindrome.
+ * This is where we'll be greedy: Iterate through the string, if any character (except mid one) is not 'a', we can simply
+ * change it to 'a' and solve the problem already.
+ * 
+ * However, if the whole string is full of 'a', we have no choice but to change the last 'a' to 'b'
+ */
+
+public class Break_a_Palindrome {
+	
+	public String breakPalindrome(String palindrome) {
+		final int len = palindrome.length();
+		int mid = len / 2;
+		// Edge case - Length 1 always palindrome
+		if (len <= 1) return "";
+		
+		for (int i = 0; i < len; ++i) {
+			// Ignore the middle character if this is odd. Changing middle char does not make it palindrome
+			if (len % 2 != 0 && i == mid) continue;
+			if (palindrome.charAt(i) == 'a') continue;
+			// Located a non 'a' character. Change it to 'a' and job is done
+			return palindrome.substring(0, i) + 'a' + palindrome.substring(i+1);
+		}
+		// String full of 'a'. Change last 'a' to 'b'
+		return palindrome.substring(0, len-1) + 'b';
+    }
+	
+}
