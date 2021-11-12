7cd5f7e1b1df43d7f86fb68c03033db5514069a2
AdmiJW <jamweg22@gmail.com>
2021-11-12 12:27:31 +0800

    Added Remove Linked List Elements

--- LeetCode/src/Linked_List/Remove_Linked_List_Elements.java
+++ LeetCode/src/Linked_List/Remove_Linked_List_Elements.java
@@ -1,4 +1,5 @@
 package Linked_List;
+import Linked_List.ListNode;
 
 //https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
 
@@ -8,6 +9,9 @@ package Linked_List;
  * 	However, careful of edge cases where the head of the linked list itself is the element to be deleted. In that case, you need the initialization iteration
  * 	to set the head to first non-ToBeDeleted node.
  * 
+ * 	Of course, we should know about dummy node technique. Instead of checking the node's next element, we can keep reference to previous node, so that when current
+ * 	node is to be removed, we simply relink the previous node's next. This is easier to code
+ * 
  * 	For recursion approach, we'll do it from the last node of the linked list. We recurse down the linked list until it is null, which we will return null.
  * 	We have to check if the current sub-Linked-list's head is the toBeDeleted node. If yes, we just return the recursed back head. Else we link the head node to
  * 	the recursed back head and return this head.
@@ -15,38 +19,28 @@ package Linked_List;
 
 public class Remove_Linked_List_Elements {
 	
-	class ListNode {
-	      int val;
-	      ListNode next;
-	      ListNode(int x) {
-	          val = x;
-	          next = null;
-	      }
-	 }
-	
+	// Iterative, Dummynode technique
 	public ListNode removeElements(ListNode head, int val) {
-		while (head != null && head.val == val) {
-			head = head.next;
-		}
-		
-		ListNode curr = head;
-		while (curr != null) {
-			if (curr.next != null && curr.next.val == val) {
-				curr.next = curr.next.next;
-			}
-			else
-				curr = curr.next;
-		}
-		return head;
+		ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+
+        for (ListNode curr = head; curr != null; curr = curr.next) {
+            if (curr.val != val) {
+                prev.next = curr;
+                prev = curr;
+            }
+        }
+        prev.next = null;
+        return dummy.next;
 	}
 	
+	
+	// Recursive approach
 	public ListNode removeRecurse(ListNode head, int val) {
 		if (head == null) return null;
+		if (head.val == val) return removeRecurse(head.next, val);
 		
-		ListNode next = removeRecurse(head.next, val);
-		
-		if (head.val == val) return next;
-		head.next = next;
+		head.next = removeRecurse(head.next, val);
 		return head;
 	}
 }
