76d13f6c7d5dbb9ca581eabe80264ae3b5238ea9
AdmiJW <jamweg22@gmail.com>
2021-11-15 12:50:22 +0800

    Added Largest Divisible Subset

--- LeetCode/src/Medium/Largest_Divisible_Subset.java
+++ LeetCode/src/Medium/Largest_Divisible_Subset.java
@@ -7,38 +7,93 @@ import java.util.List;
 //https://leetcode.com/problems/largest-divisible-subset/
 
 /*
+ * 	This is a Dynamic Programming, Sorting problem
  * 	The intuition is from the DP question Largest Increasing Subsequence (LIS).
- *	First the array has to be sorted. In that way we ensure that the remainder checking is done easily.
- *	First we pick a index i starting from 0. We'll be generating the largest set for this element by checking the elements before i.
- *	We check if the checked element j is divisible by i. If yes, we compare the lengths of the subset from answer of j to the answer of i so far.
- *	If subset of j is longer, we'll overwrite the subset stored at i. This continues until j meet with i, which the element at i is appended to
- *	the subset, resulting in the longest divisible subset for the subproblem (Array until i).
+ * 
+ * 	Let's derive some mathematic properties first in order to solve this problem:
+ * 	In order for two numbers a and b to divide properly, it must be this condition:
+ * 	
+ * 		a = nb		(a is n times of b)
+ * 
+ * 	Therefore, according to the problem, a set of numbers which every two number a and b must be able to divide each other, 
+ * 	must all be multiples of one another! See:
+ * 		{ 1, 4, 8, 32, 128 }...
+ * 	128 is four times 32, which is also four times 8, which is two times 4, which is four times 1
+ * 	
+ * 	This lead us to one conclusion: If we already have such valid divisible set, then to determine whether a new element e
+ * 	can fit inside this set or not:
+ * 	>	We simply take the largest element from the set, and check if it is divisible or not. IF YES, then we can safely
+ * 		insert new element e into the set, which is proved to be multiple of any number in the set.
+ * 
+ * 	Taking example from previous: { 1, 4, 8, 32, 128 }
+ * 
+ * 	Can 256 fit inside? We check the largest element: 128. 256 is divisible by 128. OK it is valid!
+ * 	Can 192 fit inside? We check the largest element: 128. 192 is NOT divisible by 128. NO we cannot!
+ * 	
+ * 	However, this means we always need to access the largest element in the set quickly, and also process the numbers
+ * 	in ascending order.
+ * 	(Could be not required, but makes job easier as there's no need for comparing larger element)
+ * 
+ * 	=====================================================================================
+ * 
+ * 	Therefore, we first sort the array.
+ * 	For each of the element e, it could possibly pair with any past elements, which may already belong in a subset.
+ * 	
+ * 	DP[i] records two things: 
+ * 		{
+ * 			The previous index of element that is also in the same largest subset as this element,
+ * 			The maximum length of subset it belongs to
+ * 		}
+ * 
+ * 	For each of the element e, initially it can form a subset by itself. Therefore the dp = {-1, 1}.
+ * 
+ * 	-1 means there is no previous index of element that is also in largest subset. 1 is the length of subset, which is formed
+ * 	by e itself.
+ * 
+ * 	Then, traverse backwards to find the maximum length subset, which:
+ * 		- num[i] % num[j] == 0										(Divisible)
+ * 		- dp[j].lengthOfSubset + 1 > dp[i].lengthOfSubset			(When added to that subset, will create larger subset)
+ * 	Record the optimal value in dp.
+ * 	
+ * 	At the end , you should have the index of the tail element in the largest subset. Since parent element index is recorded,
+ * 	we can follow the path to retrieve the subset itself.
  */
 
 public class Largest_Divisible_Subset {
 	public List<Integer> largestDivisibleSubset(int[] nums) {
-		if (nums.length == 0) return new ArrayList<Integer>();
-		
-		List<Integer>[] dp = new ArrayList[nums.length];
-		
-		Arrays.sort(nums);
-		
-		for (int i = 0; i < dp.length; i ++ ) {
-			dp[i] = new ArrayList<>();
-			for (int j = 0; j < i; j++ ) {
-				if (nums[i] % nums[j] == 0) {
-					if (dp[j].size() > dp[i].size() )
-						dp[i] = new ArrayList<>(dp[j]);
-				}
-			}
-			dp[i].add(nums[i] );
-		}
-		
-		List<Integer> result = dp[0];
-		for (List<Integer> i: dp) {
-			if (i.size() > result.size())
-				result = i;
-		}
-		return result;
+		final int len = nums.length;
+		int[][] dp = new int[len][2];
+        int maxIndex = -1;
+        int maxLength = 0;
+
+        // 1 - Sort to process the numbers from smallest to largest
+        Arrays.sort(nums);
+
+        // 2 - For each number, find largest subset it can belong to, and record in dp
+        for (int i = 0; i < len; ++i) {
+            dp[i][0] = -1; 
+            dp[i][1] = 1;
+
+            // Traverse backwards to find the dp solution for this iteration
+            for (int j = i - 1; j != -1; --j) {
+                if (nums[i] % nums[j] == 0 && dp[j][1] + 1 > dp[i][1]) {
+                	dp[i][0] = j;
+                	dp[i][1] = dp[j][1] + 1;
+                }
+            }
+
+            // Update max
+            if (dp[i][1] > maxLength) {
+                maxIndex = i;
+                maxLength = dp[i][1];
+            }
+        }
+
+
+        // 3 - Construct result
+        List<Integer> res = new ArrayList<>();
+        for (int i = maxIndex; i != -1; i = dp[i][0])
+            res.add(nums[i]);
+        return res;
 	}
 }
