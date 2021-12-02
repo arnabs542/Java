7e862f8fa869a52a6992c15d86fef49649446f90
AdmiJW <jamweg22@gmail.com>
2021-12-02 12:50:10 +0800

    Added Odd Even Linked List

--- LeetCode/src/Medium/Odd_Even_Linked_List.java
+++ LeetCode/src/Medium/Odd_Even_Linked_List.java
@@ -2,14 +2,27 @@ package Medium;
 
 
 //https://leetcode.com/problems/odd-even-linked-list/
+/*
+*	This is a linked list problem.
+* 
+*	With my idea, simply construct two linked list from existing nodes: Oddlist and Evenlist. Then at the end, connect
+*	them two and return the combined linked list.
+* 
+*	Using the concept of dummy node, everything can be done much more easier and readable way.
+* 
+*	Keep track of 4 items: oddHead, oddTail, evenHead, evenTail. Iterate through the original linked list and append them
+*	to oddTail or evenTail accordingly. 
+*	At the end, connect oddTail to evenHead (Not the dummy node), and point evenTail to null. Return the oddHead (Not the dummy node)
+* 
+*/
+
 
 public class Odd_Even_Linked_List {
 
 	class ListNode {
 		int val;
 		ListNode next;
-		ListNode() {
-		}
+		ListNode() {}
 		ListNode(int val) {
 			this.val = val;
 		}
@@ -18,116 +31,30 @@ public class Odd_Even_Linked_List {
 			this.next = next;
 		}
 	}
-	/*
-	 * 	This is a mistake because I arranged them based on the val of each node, not based on the indexing of node itself
-	 */
-//	public ListNode oddEvenList(ListNode head) {
-//		if (head == null) return null;
-//		
-//		ListNode evenHead = null;
-//		ListNode evenTail = null;
-//		
-//		while (head != null && head.val % 2 == 0) {
-//			if (evenHead == null) {
-//				evenHead = head;
-//				evenTail = head;
-//			}
-//			else
-//				evenTail.next = head;
-//			head = head.next;
-//		}
-//
-//		if (head == null) return evenHead;
-//		
-//		ListNode curr = head;
-//		
-//		while (curr.next != null ) {
-//			if (curr.next.val % 2 == 0) {
-//				if (evenHead == null) {
-//					evenHead = curr.next;
-//					evenTail = curr.next;
-//				}
-//				else {
-//					evenTail.next = curr.next;
-//					evenTail = evenTail.next;
-//				}
-//				curr.next = curr.next.next;
-//			}
-//			else {
-//				curr = curr.next;
-//			}
-//		}
-//		evenTail.next = null;
-//		curr.next = evenHead;
-//		return head;
-//	}
 	
 
 	public ListNode oddEvenList(ListNode head) {
-		if (head == null) return null;
-		if (head.next == null) return head;
-		
-		ListNode currentNode = head;
-		ListNode evenHead = null;
-		ListNode lastOdd = head;
-		ListNode lastEven = null;
-		
-		//We will be starting with the second node
-		boolean isEven = true;
-		
-		while (currentNode.next != null) {
-			currentNode = currentNode.next;
-			
-			//Currently we are at evenly numbered node
-			if (isEven) {
-				if (evenHead == null) {
-					evenHead = currentNode;
-					lastEven = currentNode;
-				}
-				else {
-					lastEven.next = currentNode;
-					lastEven = currentNode;
-				}
-			}
-			//Else, we are currently at a odd numbered node
-			else {
-				lastOdd.next = currentNode;
-				lastOdd = currentNode;
-			}
-			
-			isEven = !isEven;
-		}
-		
-		lastOdd.next = evenHead;
-		lastEven.next = null;
-		return head;
-	}
-	
-	public ListNode oddEvenListSecondTry(ListNode head) {
-		if (head == null || head.next == null) return head;
-		
-		ListNode evenHead = head.next;
+		ListNode oddHead = new ListNode();
+		ListNode evenHead = new ListNode();
+		ListNode oddTail = oddHead;
 		ListNode evenTail = evenHead;
-		head.next = head.next.next;
-		
-		ListNode curr = head;
-		int index = 3;
-		while (curr.next != null) {
-			if (index % 2 != 0) {
-				curr = curr.next;
+
+		for (int i = 1; head != null; ++i, head = head.next) {
+			// Odd
+			if ( (i & 1) == 1 ) {
+				oddTail.next = head;
+				oddTail = oddTail.next;
 			}
+			// Even
 			else {
-				evenTail.next = curr.next;
+				evenTail.next = head;
 				evenTail = evenTail.next;
-				curr.next = curr.next.next;
 			}
-			
-			index++;
 		}
-		
+
 		evenTail.next = null;
-		curr.next = evenHead;
-		return head;
+		oddTail.next = evenHead.next;
+		return oddHead.next;
 	}
 	
 }
