41d429cdd40e6d867a4c40b1fb8889d478def935
AdmiJW <jamweg22@gmail.com>
2021-12-13 10:50:58 +0800

    Added Consecutive Characters

--- LeetCode/src/Easy/Consecutive_Characters.java
+++ LeetCode/src/Easy/Consecutive_Characters.java
@@ -7,15 +7,16 @@ package Easy;
 * 
 *	Given a non empty string, the result must always be greater than or equal to 1. 1 is minimum because any character
 *	already counts.
+*
 *	Starting from second character, scan until last character. If the previous character is same as current character,
 *	add 1 to power. Otherwise, reset the power.
+*
 *	After updating the power in each iteration, compare the power to the result, so that result stores the maximum power
 *	met.
 */
 
 public class Consecutive_Characters {
     public int maxPower(String s) {
-    	if (s.isEmpty()) return 0;
     	int res = 1;
     	int count = 1;
     	for (int i = 1; i < s.length(); i ++ ) {
