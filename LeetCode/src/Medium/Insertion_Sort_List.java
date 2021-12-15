602b15eb7fed77f824d27d7742b6056994c812c0
AdmiJW <jamweg22@gmail.com>
2021-12-15 10:26:12 +0800

    Added Insertion sort list

--- LeetCode/src/Medium/Insertion_Sort_List.java
+++ LeetCode/src/Medium/Insertion_Sort_List.java
@@ -1,16 +1,16 @@
 package Medium;
 
 //https://leetcode.com/problems/insertion-sort-list/
-
 /*
  * 	This is, obviously a insertion sort problem, with linked list.
  * 
  * 	The basic idea of insertion sort is that we keep trying to insert elements one by one into a partially sorted
  * 	linked list. Do this with every element, then the whole list will be sorted at the end.
  * 
- * 	For this to be achieved in a linked list, we can have a fake head pointer which shall point at the start of the
+ * 	For this to be achieved in a linked list, we can have a sorted head pointer which shall point at the start of the
  * 	sorted linked list. Initially, it will be empty but hopefully at the end, it will point to head of fully sorted
  * 	linked list.
+ * 	
  * 	Therefore, we will be expanding this supposedly sorted linked list one by one element. From the original, unsorted
  * 	list, we will be trying to insert the element into the partially sorted linked list. This is done by just having this
  * 	element and iterate through the partially sorted linked list until it found its supposed place.
@@ -28,10 +28,11 @@ package Medium;
  *	The recursive works by first recurse until the end of the list, and returning the partially sorted list from the
  *	backwards. Then on the current node, try to insert the node into the partially sorted list, and return the
  *	head of partially sorted list back up one recursion layer
- *	
  */
 
+
 import Linked_List.ListNode;
+
 
 public class Insertion_Sort_List {
 	
@@ -50,9 +51,8 @@ public class Insertion_Sort_List {
 		
 		//	Otherwise the head of partially sorted linked list won't change. Insert current node and return head
 		ListNode curr = sorted;
-		while (curr.next != null && curr.next.val < head.val) {
+		while (curr.next != null && curr.next.val < head.val)
 			curr = curr.next;
-		}
 		
 		ListNode temp = curr.next;
 		curr.next = head;
@@ -62,46 +62,35 @@ public class Insertion_Sort_List {
     }
 	
 	
-	// ITERATIVE METHOD O(N) space complexity
-	public ListNode insertionSortListIterative( ListNode head ) {
-		if (head == null) return null;
+	
+	// Iterative O(N^2) time O(1) space solution
+	public ListNode insertionSortList2(ListNode head) {
+		ListNode sortedHead = head;
+		head = head.next;
+		sortedHead.next = null;
 		
-		ListNode sortedHead = null;		//	Initially there should be nothing in partially sorted linked list
-		ListNode toInsert = head;		//	The element to be insert
-		ListNode temp = toInsert;		//	The next element in line to be inserted. Upon entering while loop, it will
-										//	become the current element's next
-		
-		//	While the nodes are not yet finish inserted
-		while (toInsert != null) {
-			temp = temp.next;
-			sortedHead = insertion( toInsert, sortedHead );
-			toInsert = temp;
+		// Now for every element in the unsorted linked list, try to insert into sorted linked list
+		while (head != null) {
+			ListNode next = head.next;
+			
+			// Case 1 - If this node should be inserted at first element
+			if (head.val < sortedHead.val) {
+				head.next = sortedHead;
+				sortedHead = head;
+			}
+			// General case - Insertion sort to find location to insert
+			else {
+				ListNode toIns = sortedHead;
+				while (toIns.next != null && toIns.next.val < head.val)
+					toIns = toIns.next;
+				head.next = toIns.next;
+				toIns.next = head;
+			}
+			
+			// Move head pointer to head of unsorted linked list
+			head = next;
 		}
 		
 		return sortedHead;
-		
 	}
-	
-	//	Given the node and head of partially sorted linked list, insert the node and return head of sorted linked list
-	public ListNode insertion( ListNode nodeToInsert, ListNode sortedHead) {
-		
-		//	If current node was the head, then connect and return this node
-		if (sortedHead == null || nodeToInsert.val <= sortedHead.val ) {
-			nodeToInsert.next = sortedHead;
-			return nodeToInsert;
-		}
-		
-		//	Otherwise the head of partially sorted linked list won't change. Insert current node and return head
-		ListNode curr = sortedHead;
-		while (curr.next != null && curr.next.val < nodeToInsert.val) {
-			curr = curr.next;
-		}
-		
-		ListNode temp = curr.next;
-		curr.next = nodeToInsert;
-		nodeToInsert.next = temp;
-		
-		return sortedHead;
-	}
-	
 }
